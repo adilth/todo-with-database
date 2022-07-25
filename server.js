90d5441ef73d15f5701592528025b7796be0ae68
adil tahiri <rajaadil1995@gmail.com>
2022-07-25 01:24:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+
+mongoose.connect(process.env.CONNECT_DB, { useNewUrlParser: true }, () =>
+  console.log("Connect to DB")
+);
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.listen(PORT, () => console.log("Connected to port "));
