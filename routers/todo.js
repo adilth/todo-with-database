e3b100d00710440fb994beeff7b49cd3c760019f
adil tahiri <rajaadil1995@gmail.com>
2022-08-01 22:12:34 +0100

    add update the task

--- routers/todo.js
+++ routers/todo.js
@@ -11,3 +11,43 @@ router.get("/", async (req, res) => {
     res.status(500).send(err);
   }
 });
+router.post("/", async (req, res) => {
+  // const { title, content } = req.body
+  const tasks = await new Todo({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  try {
+    await tasks.save();
+    console.log(tasks);
+    res.redirect("/");
+  } catch (e) {
+    res.status(501).send(e);
+  }
+});
+
+//edite or update tasks
+
+router
+  .route("/edite/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    Todo.find({}, (err, tasks) => {
+      if (err) console.log(err);
+      res.render("edite.ejs", { todoTasks: tasks, taskId: id });
+    });
+  })
+  .post((req, res) => {
+    const { id } = req.params;
+    Todo.findByIdAndUpdate(
+      id,
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      (err) => {
+        if (err) return res.status(501).send(err);
+        res.redirect("/");
+      }
+    );
+  });
