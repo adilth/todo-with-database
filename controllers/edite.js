5196046901a77b8020c8c49614db2d1f33b1264e
adil tahiri <rajaadil1995@gmail.com>
2022-08-27 23:09:20 +0100

    add edite page controller

--- controllers/edite.js
+++ controllers/edite.js
@@ -1,1 +1,32 @@
 const Todo = require("../models/Todo");
+
+module.exports = {
+  getEditeTask: (req, res) => {
+    const { id } = req.params;
+    Todo.find({}, (err, tasks) => {
+      if (err) console.log(err);
+      res.render("edite.ejs", { todoTasks: tasks, taskId: id });
+    });
+  },
+  postEditeTask: (req, res) => {
+    const { id } = req.params;
+    Todo.findByIdAndUpdate(
+      id,
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      (err) => {
+        if (err) return res.status(501).send(err);
+        res.redirect("/");
+      }
+    );
+  },
+  deleteTask: (req, res) => {
+    const id = req.params.id;
+    Todo.findByIdAndRemove(id, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect("/");
+    });
+  },
+};
