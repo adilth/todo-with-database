f4276f65a2da9c01493f60e924433fd92bf2b0c1
adil tahiri <rajaadil1995@gmail.com>
2022-08-28 01:45:46 +0100

    add todo page controller

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,1 +1,26 @@
 const Todo = require("../models/Todo");
+
+module.exports = {
+  getTodo: async (req, res) => {
+    try {
+      const tasks = await Todo.find();
+      res.render("index.ejs", { todoTasks: tasks });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
+  createTodo: async (req, res) => {
+    // const { title, content } = req.body
+    const tasks = await new Todo({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    try {
+      await tasks.save();
+      console.log(tasks);
+      res.redirect("/");
+    } catch (e) {
+      res.status(501).send(e);
+    }
+  },
+};
