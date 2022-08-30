82b390ba97a765bdc1cceb64792718ed2d5d261e
adil tahiri <rajaadil1995@gmail.com>
2022-08-31 00:03:15 +0100

    remove some not need code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const todo = require("./routers/todo");
 const edite = require("./routers/edite");
+// const path = require("path");
 const PORT = process.env.PORT || 3000;
 require("dotenv").config({ path: "./config/.env" });
 const connectDb = require("./config/db");
@@ -16,14 +16,14 @@ connectDb();
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 app.use("/", todo);
 app.use("/edite", edite);
 app.listen(PORT, () => console.log("Connected to port "));
