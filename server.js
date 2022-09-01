c31d7fc86820166176afad90600939b518a24b83
adil tahiri <rajaadil1995@gmail.com>
2022-09-01 22:10:39 +0100

    fix code

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const todo = require("./routers/todo");
 const edite = require("./routers/edite");
-// const path = require("path");
 const PORT = process.env.PORT || 3000;
 require("dotenv").config({ path: "./config/.env" });
 const connectDb = require("./config/db");
@@ -18,12 +17,14 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use("/", todo);
 app.use("/edite", edite);
+
 app.listen(PORT, () => console.log("Connected to port "));
